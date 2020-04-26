6fdca2296e344ae272f3ddd2a4411194b8b48fab
Shreddan <40694103+Shreddan@users.noreply.github.com>
2020-04-26 23:17:15 +0100

    Time sorted.

--- MLAK-Clone/Engine.cpp
+++ MLAK-Clone/Engine.cpp
@@ -23,20 +23,35 @@ bool Engine::OnUserUpdate(float fElapsedTime)
 
 void Engine::Time(float fElapsedTime)
 {
-	DrawStringDecal(olc::vf2d(350, 200), game.getHour, olc::RED);
+	DrawStringDecal(olc::vf2d(250, 200), "Day : " + std::to_string(game.getDay()), olc::RED, olc::vf2d(2.f, 2.f));
+	DrawStringDecal(olc::vf2d(250, 300), "Hour : " + std::to_string(game.getHour()), olc::RED, olc::vf2d(2.f, 2.f));
 	timeTick += fElapsedTime;
 
-	if (game.getTA() == 1)
+	switch (game.getTA())
 	{
-		time = 
+		case 1:
+		{
+			time = 25.f;
+			break;
+		}
+		case 2:
+		{
+			time = 35.f;
+		}
+		case 3:
+		{
+			time - 50.f;
+		}
 	}
+	
 
 	if (timeTick >= time)
 	{
 		game.incremHour();
 
-		if (game.getHour() == 20)
+		if (game.getHour() > 20)
 		{
+			game.incrementDay();
 			game.setHour(8);
 		}
 
