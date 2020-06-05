b5166ce28cb5127307726fd1aa9925cb5ad76a88
Shreddan <40694103+Shreddan@users.noreply.github.com>
2020-06-05 14:27:07 +0100

    Dungeon class groundwork - will probably edit this in future

--- MLAK-Clone/Dungeon.cpp
+++ MLAK-Clone/Dungeon.cpp
@@ -1,9 +1,56 @@
 #include "Dungeon.h"
 
-Dungeon::Dungeon()
+Dungeon::Dungeon(int Diff)
 {
+	this->Difficulty = Diff;
+	setNumEnemies();
 }
 
 Dungeon::~Dungeon()
 {
+}
+
+void Dungeon::setNumEnemies()
+{
+	switch (Difficulty)
+	{
+		case VeryEasy:
+		{
+			numEnemies = 2;
+			break;
+		}
+		case Easy:
+		{
+			numEnemies = 3;
+			break;
+		}
+		case Intermediate:
+		{
+			numEnemies = 5;
+			hasBoss = true;
+			break;
+		}
+		case Hard:
+		{
+			numEnemies = 5;
+			hasLegendary = true;
+			hasBoss = true;
+			break;
+		}
+		case VeryHard:
+		{
+			numEnemies = 6;
+			hasLegendary = true;
+			hasBoss = true;
+			break;
+		}
+		case Brutal:
+		{
+			numEnemies = 10;
+			hasLegendary = true;
+			hasBoss = true;
+			break;
+		}
+
+	}
 }
--- MLAK-Clone/Dungeon.h
+++ MLAK-Clone/Dungeon.h
@@ -12,13 +12,22 @@ enum Difficulty
 class Dungeon
 {
 public:
-	Dungeon();
+	Dungeon(int Diff);
 	~Dungeon();
 
+
+	//Get-Set
+	void setNumEnemies();
+	void setMateriaAm(int i) { materiaAmount = i; }
+	void setActive(bool active) { isActive = active; }
 
 
 private:
 	int Difficulty;
-
+	int numEnemies;
+	int materiaAmount;
+	bool isActive;
+	bool hasLegendary;
+	bool hasBoss;
 };
 
