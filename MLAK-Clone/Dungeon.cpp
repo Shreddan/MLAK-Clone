a088280bbbd529850520e24370352febd776702c
Shreddan <40694103+Shreddan@users.noreply.github.com>
2020-06-05 14:31:49 +0100

    forgot to add a thing

--- MLAK-Clone/Dungeon.cpp
+++ MLAK-Clone/Dungeon.cpp
@@ -17,22 +17,26 @@ void Dungeon::setNumEnemies()
 		case VeryEasy:
 		{
 			numEnemies = 2;
+			setMateriaAm(20);
 			break;
 		}
 		case Easy:
 		{
 			numEnemies = 3;
+			setMateriaAm(40);
 			break;
 		}
 		case Intermediate:
 		{
 			numEnemies = 5;
+			setMateriaAm(60);
 			hasBoss = true;
 			break;
 		}
 		case Hard:
 		{
 			numEnemies = 5;
+			setMateriaAm(100);
 			hasLegendary = true;
 			hasBoss = true;
 			break;
@@ -40,6 +44,7 @@ void Dungeon::setNumEnemies()
 		case VeryHard:
 		{
 			numEnemies = 6;
+			setMateriaAm(150);
 			hasLegendary = true;
 			hasBoss = true;
 			break;
@@ -47,6 +52,7 @@ void Dungeon::setNumEnemies()
 		case Brutal:
 		{
 			numEnemies = 10;
+			setMateriaAm(300);
 			hasLegendary = true;
 			hasBoss = true;
 			break;
